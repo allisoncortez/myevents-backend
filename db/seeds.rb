4c0d8ab02dbe2d961138dcb5fc3202c30dc93e43
allisoncortez <allizetorc@gmail.com>
2020-08-07 15:39:49 -0500

    migrations tested in console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Comment.destroy_all
 Event.destroy_all
 
 
 artShow = Event.create(
+    
     title:"Monet",
     description: "free art show on the life of Monet and his work",
-    # starttime,endtime
+    startTime: Date.strptime("20071119T0837", "%Y%m%dT%H%M"),
+    endTime: Date.strptime("12/13/2013", "%m/%d/%Y"),
     category: "art",
     location: "Museum of Fine Art"
     )
+
+    concert = Event.create(
+    title:"Courtney Bartnett",
+    description: "free show, spaces limited",
+    startTime: Date.new(20021, 01, 10),
+    endTime: Date.new(2021, 1, 10),
+    category: "music",
+    location: "HRC"
+    )
+
+newComment = Comment.create(description: "description", event_id:1)
 
 # concert = Event.create()
 # danceContest = Event.create()
