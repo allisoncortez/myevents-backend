75978402f7f3ae36c027fefb5bea44c6fa1c3f12
allisoncortez <allizetorc@gmail.com>
2020-08-07 21:39:37 -0500

    notes on comments controller and routes, commented out for now

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::CommentsController < ApplicationController
+
+    # before_action :set_event
+    # # api/v1/events/1/comments
+    # def index
+    #     comments = @event.comments
+    #     render json: comments
+    # end
+
+    # def create 
+    #     comment = @event.comments.build(comment_params)
+    #     if comment.save
+    #         render json: comment, status: :accepted
+    #     end
+    # end
+
+    # private 
+
+    # def set_event
+    #     @event = Event.find(params[:event_id])
+    # end
+
+    # def comment_params
+    #     params.require(:comment).permit(:description, :event_id)
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :events, only: [:index, :create, :show]
-      # resources :comments
+      resources :events, only: [:index, :create, :show] # do
+        # resources :comments, only[:index, :create]
+      # end
     end
   end
 end
